628e5202b6f4e222ffe824da2c70105a95cef157
sktakyi <s.k.takyi5@gmail.com>
2024-07-28 01:22:50 +0000

    Initial-Commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,13 +5,13 @@ USE alx_book_store;
 -- Create the Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(100) NOT NULL
+    author_name VARCHAR(215) NOT NULL
 );
 
 -- Create the Books table
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
+    title VARCHAR(215) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
@@ -21,8 +21,8 @@ CREATE TABLE Books (
 -- Create the Customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
