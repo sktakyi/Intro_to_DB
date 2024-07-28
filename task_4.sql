e0b635b4141c1534429667a6407523480685b5e6
sktakyi <s.k.takyi5@gmail.com>
2024-07-28 14:27:40 +0000

    Initial-Commit

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Get the full description of the books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
